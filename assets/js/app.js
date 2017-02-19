0fa3ecb7bb1589cfb4ae5da422415552f69b5343
JonathanDempsey <jonathanedempsey@gmail.com>
2017-02-19 17:23:31 +0000

    Landing Page and Navbar
    - HTML template of the landing page and navbar.
    - Navbar animations for the logo and links.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 console.log("Bye!");
+
+/* Navbar */
+var navbar = $('#navbar-main'),
+distance = navbar.offset().top,
+$window = $(window);
+
+$window.scroll(function() {
+  if ($window.scrollTop() >= distance) {
+    navbar.removeClass('navbar--fixed-top').addClass('navbar--fixed-top');
+  } else {
+    navbar.removeClass('navbar--fixed-top');
+  }
+});
