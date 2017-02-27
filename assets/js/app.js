19559a19ec9554998fb0c541f4dd4c956c9dc3f8
JonathanDempsey <jonathanedempsey@gmail.com>
2017-02-27 22:54:21 +0000

    Portfolio Section Rough #2
    - Text content is now sticky after the first project background.
    - Added appropriate amount of backgrounds for number of projects.
    - Note: Sort out the div/section naming and ordering of the portolio
    HTML.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,15 @@
-// Sticky navbar.
+// Sticky navbar and portfolio content.
 var navbar = $('#navbar-main'),
-distance = navbar.offset().top,
-$window = $(window);
+    port = $('.portfolio-content'),
+    distance = navbar.offset().top,
+    $window = $(window);
 
 $window.scroll(function() {
   if ($window.scrollTop() >= distance) {
     navbar.removeClass('navbar--fixed-top').addClass('navbar--fixed-top');
+    port.removeClass('portfolio-content--fixed-top').addClass('portfolio-content--fixed-top');
   } else {
     navbar.removeClass('navbar--fixed-top');
+    port.removeClass('portfolio-content--fixed-top');
   }
 });
